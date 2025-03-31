db878f8d9846c4816ae9772e1844b3880f51f295
Emerson <emersonnjunior2006@gmail.com>
2025-03-31 14:04:19 -0300

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,26 @@ links.forEach(link => {
         });
     });
 });
+// Seleciona todos os elementos com a classe 'fade-in-up'
+const fadeInElements = document.querySelectorAll('.fade-in-up');
+
+// Função que verifica se o elemento está visível na tela
+function checkVisibility() {
+    fadeInElements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        
+        // Verifica se o elemento está dentro da área visível
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            // Se o elemento não tiver a classe 'visible', aplica a animação
+            if (!element.classList.contains('visible')) {
+                element.classList.add('visible');
+            }
+        }
+    });
+}
+
+// Verifica a visibilidade dos elementos ao rolar a página
+window.addEventListener('scroll', checkVisibility);
+
+// Chama a função inicialmente para adicionar os efeitos em elementos que já estão visíveis
+checkVisibility();
