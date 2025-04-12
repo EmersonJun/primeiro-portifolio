c69619aa248036c051ded1fef8ea1de4725e0a18
Emerson <emersonnjunior2006@gmail.com>
2025-04-12 12:15:52 -0300

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,28 @@ window.addEventListener('scroll', checkVisibility);
 
 // Chama a função inicialmente para adicionar os efeitos em elementos que já estão visíveis
 checkVisibility();
+
+
+
+
+
+
+
+
+
+
+let currentIndex = 0;
+
+  function scrollCarousel(direction) {
+    const track = document.getElementById('carouselTrack');
+    const itemWidth = track.querySelector('.work-item').offsetWidth + 0; // 20 = padding
+    const totalItems = track.children.length;
+    const maxIndex = totalItems - 3;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) currentIndex = 0;
+    if (currentIndex > maxIndex) currentIndex = maxIndex;
+
+    track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+  }
