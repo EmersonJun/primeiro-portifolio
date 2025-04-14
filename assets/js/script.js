c167be9a870e95d1d91f2303df4a09291df77777
Emerson <emersonnjunior2006@gmail.com>
2025-04-13 22:00:56 -0300

    add css

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,3 +88,27 @@ let currentIndex = 0;
       }
     });
   });
+
+
+
+
+
+
+
+
+
+
+
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    window.onscroll = () => {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            scrollTopBtn.style.display = "block";
+        } else {
+            scrollTopBtn.style.display = "none";
+        }
+    };
+
+    scrollTopBtn.onclick = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
