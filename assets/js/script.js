0e89e9e8a280d8cb56e333c0675ac56daca713eb
Emerson <emersonnjunior2006@gmail.com>
2025-04-16 13:43:55 -0300

    cell

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,11 @@
-// Selecionando todos os links de navegação
-const links = document.querySelectorAll('nav ul li a');
 
-// Adicionando o evento de clique em cada link
-links.forEach(link => {
-    link.addEventListener('click', function(e) {
-        // Previne o comportamento padrão de navegação
-        e.preventDefault();
-        
-        // Obtém o alvo (ID) do link
-        const targetId = link.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+  window.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.fade-in-up');
 
-        // Realiza a rolagem suave até o alvo
-        targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+    elements.forEach((el, index) => {
+      setTimeout(() => {
+        el.classList.add('active');
+      }, index * 100); // Efeito em cascata, 100ms entre cada
     });
-});
-// Seleciona todos os elementos com a classe 'fade-in-up'
-const fadeInElements = document.querySelectorAll('.fade-in-up');
-
-// Função que verifica se o elemento está visível na tela
-function checkVisibility() {
-    fadeInElements.forEach(element => {
-        const rect = element.getBoundingClientRect();
-        
-        // Verifica se o elemento está dentro da área visível
-        if (rect.top < window.innerHeight && rect.bottom >= 0) {
-            // Se o elemento não tiver a classe 'visible', aplica a animação
-            if (!element.classList.contains('visible')) {
-                element.classList.add('visible');
-            }
-        }
-    });
-}
-
-// Verifica a visibilidade dos elementos ao rolar a página
-window.addEventListener('scroll', checkVisibility);
-
-// Chama a função inicialmente para adicionar os efeitos em elementos que já estão visíveis
-checkVisibility();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
+  });
 
